801858dc2a6bba39307b91ed2c4e23da6b1b85dc
20721 <freely@vip.qq.com>
2026-03-20 16:43:54 +0800

    Add third-party software sources in feeds.conf.default
    Add third-party software sources to feeds configuration

---
+++ diy-part1.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+# 追加第三方软件源
+cat >> feeds.conf.default <<'EOF'
+
+# OpenClash
+src-git openclash https://github.com/vernesong/OpenClash
+
+# Turbo ACC
+src-git turboacc https://github.com/chenmozhijin/turboacc.git;luci
+
+# AdGuard Home LuCI（本体 adguardhome 走官方包，这里只补 LuCI）
+src-git adguardhome_luci https://github.com/rufengsuixing/luci-app-adguardhome
+EOF
