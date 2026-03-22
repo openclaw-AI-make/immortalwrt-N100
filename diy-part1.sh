dd07c02000c1d6bc253be9472d314c2e23555366
20721 <freely@vip.qq.com>
2026-03-22 12:58:31 +0800

    Fix: 修复 feeds 配置顺序

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,20 +2,16 @@
 set -e
 
 # 追加第三方软件源
-cat >> feeds.conf.default <<'FEEDS_EOF'
+cat >> feeds.conf.default <<'EOF'
 
 # OpenClash
 src-git openclash https://github.com/vernesong/OpenClash
 
-# Turbo ACC
+# Turbo ACC  
 src-git turboacc https://github.com/chenmozhijin/turboacc.git;luci
 
-# AdGuard Home - 使用 immortalwrt 官方 feed
+# AdGuard Home feeds
 src-git packages https://github.com/immortalwrt/packages
 src-git luci https://github.com/immortalwrt/luci
-FEEDS_EOF
+EOF
 
-# 克隆 AdGuard Home 相关包
-cd feeds/luci/applications
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git 2>/dev/null || true
-cd ../../../
