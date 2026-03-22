e96ba44331e41ed932a698a1f69f0a45535d9077
20721 <freely@vip.qq.com>
2026-03-22 13:07:13 +0800

    简化 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,8 +7,3 @@ sed -i 's/192.168.1.1/192.168.10.4/g' package/base-files/files/bin/config_genera
 # 修改主机名
 sed -i 's/hostname='ImmortalWrt'/hostname='ImmortalWrt-x86'/g' package/base-files/files/bin/config_generate || true
 
-# 克隆 AdGuard Home LuCI 到 feeds
-cd feeds/luci/applications
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git 2>/dev/null || true
-cd ../../../../
-
