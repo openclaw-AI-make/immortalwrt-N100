50eb58861cf423489c9656b6574b34ba4067857f
20721 <freely@vip.qq.com>
2026-03-23 08:14:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,8 +2,18 @@
 set -e
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.10.4/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.14/g' package/base-files/files/bin/config_generate
 
 # 修改主机名
-sed -i 's/hostname='ImmortalWrt'/hostname='ImmortalWrt-x86'/g' package/base-files/files/bin/config_generate || true
+sed -i "s/hostname='ImmortalWrt'/hostname='ImmortalWrt-x86'/g" package/base-files/files/bin/config_generate || true
 
+# 追加需要的包选择
+cat >> .config <<'EOF'
+CONFIG_PACKAGE_luci-compat=y
+CONFIG_PACKAGE_rpcd=y
+CONFIG_PACKAGE_uhttpd=y
+CONFIG_PACKAGE_adguardhome=y
+CONFIG_PACKAGE_luci-app-adguardhome=y
+CONFIG_PACKAGE_luci-app-passwall=y
+CONFIG_PACKAGE_xray-core=y
+EOF
