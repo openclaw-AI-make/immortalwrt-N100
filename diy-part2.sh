cd61336187dd8bb32a6861eb0b511945b84934f7
20721 <freely@vip.qq.com>
2026-03-23 12:01:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,3 +6,17 @@ sed -i 's/192.168.1.1/192.168.10.14/g' package/base-files/files/bin/config_gener
 
 # 修改主机名
 sed -i "s/hostname='ImmortalWrt'/hostname='ImmortalWrt-x86'/g" package/base-files/files/bin/config_generate || true
+
+# 修正 luci-app-adguardhome 在新系统下的部分依赖/兼容问题
+if [ -f package/luci-app-adguardhome/Makefile ]; then
+  sed -ri '/^LUCI_DEPENDS:=/s#\+(ca-certs|wget-ssl)##g' package/luci-app-adguardhome/Makefile || true
+fi
+
+# 强制选中
+cat >> .config <<'EOF'
+CONFIG_PACKAGE_adguardhome=y
+CONFIG_PACKAGE_luci-app-adguardhome=y
+CONFIG_PACKAGE_luci-compat=y
+CONFIG_PACKAGE_rpcd=y
+CONFIG_PACKAGE_uhttpd=y
+EOF
