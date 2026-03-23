e9732a2056d199059fb7a31e90399a76f44957b9
20721 <freely@vip.qq.com>
2026-03-23 12:01:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-cat >> feeds.conf.default <<'EOF'
-
 # OpenClash
-src-git openclash https://github.com/vernesong/OpenClash
+git clone --depth=1 https://github.com/vernesong/OpenClash package/openclash
 
 # Turbo ACC
-src-git turboacc https://github.com/chenmozhijin/turboacc.git;luci
+git clone --depth=1 -b luci https://github.com/chenmozhijin/turboacc.git package/turboacc
 
 # AdGuard Home LuCI
-src-git adguardhome_luci https://github.com/rufengsuixing/luci-app-adguardhome
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
-# Passwall
+# Passwall feeds
+cat >> feeds.conf.default <<'EOF'
+
 src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main
 src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main
 EOF
