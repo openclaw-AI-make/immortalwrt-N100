68582ae74534ddf340e5ffb4c989098a75931e4e
20721 <freely@vip.qq.com>
2026-03-25 19:28:05 +0800

    IP 改回 192.168.10.14

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -6,7 +6,7 @@ uci set system.@system[0].hostname='ImmortalWrt-x86'
 uci set system.@system[0].timezone='CST-8'
 uci set system.@system[0].zonename='Asia/Shanghai'
 uci commit system
-uci set network.lan.ipaddr='192.168.10.4'
+uci set network.lan.ipaddr='192.168.10.14'
 uci commit network
 EOF
 chmod +x files/etc/uci-defaults/99-custom
